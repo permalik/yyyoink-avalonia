a0ad371b515456c4eaa0320db55637ea0cbbe84d
permalik <permalik@protonmail.com>
2025-06-23 22:34:37 -0500

    feat: init avalonia

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace YyyoinkAvalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
