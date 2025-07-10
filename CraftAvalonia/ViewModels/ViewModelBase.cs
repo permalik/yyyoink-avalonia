5b830aa3e1e6958b65cfb8c520d3d3b5a848f248
permalik <permalik@protonmail.com>
2025-07-09 23:16:09 -0500

    feat: init avalonia

---
+++ CraftAvalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CraftAvalonia.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ CraftAvalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CraftAvalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
